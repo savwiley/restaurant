33b5151def589fc67ce709b6c41bf1f06670b2b9
Savannaha <savwiley@yahoo.com>
2020-11-13 16:46:38 -0600

    created repo

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my Restaurant site for TOP!
---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log("I'm working!")
+
+/*
+Restaurant them ideas!
+
+Star Trek! There are tons of foods listed online.
+
+Odin! Norse/Viking sort of stuff. Make it Nordic God themed.
+
+
+
+*/

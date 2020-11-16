897bce84cbf1b88c2fbc98b94f0e51ba71555984
Savannaha <savwiley@yahoo.com>
2020-11-16 16:01:56 -0600

    tested home page tab

--- src/module/homePage.js
+++ src/module/homePage.js
@@ -1,14 +1,16 @@
-const content = document.querySelector("div#content");
+const mainContent = document.querySelector(".mainContent");
 
-const test = (() => {
+function test() {
     const testing = document.createElement("div");
-        testing.style.width = "400px";
-        testing.style.height = "400px";
-        testing.style.background = "hotpink";
-        content.appendChild(testing);
-    content.style.background = "green";
-})();
+        testing.style.width = "100px";
+        testing.style.height = "100px";
+        testing.style.background = "blue";
+        mainContent.appendChild(testing);
+    mainContent.style.background = "green";
+};
 
-
+function homePage() {
+    test();
+};
 
 export default homePage;

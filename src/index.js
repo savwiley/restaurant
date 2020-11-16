8b831e7fd4ee10ab823bff005849646fbcee4294
Savannaha <savwiley@yahoo.com>
2020-11-16 16:00:16 -0600

    attempted to attach pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import initialPageLoad from "./module/initialPageLoad";
 import homePage from "./module/homePage";
+import menuPage from "./module/menuPage";
+
+run();
 
 const run = (() => {
     initialPageLoad();
@@ -8,6 +11,8 @@ const run = (() => {
 })();
 
 function events() {
-    const homeBtn = document.querySelector("#homeBtn");
-    homeBtn.addEventListener('click', homePage);
-}
+    const homeBtn = document.getElementById("homeBtn");
+        homeBtn.addEventListener('click', homePage);
+    const menuBtn = document.getElementById("menuBtn");
+        menuBtn.addEventListener('click', menuPage);
+};

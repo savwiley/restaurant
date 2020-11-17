6d06593eb10ad65c16fd3a32db3b8503c3155946
Savannaha <savwiley@yahoo.com>
2020-11-17 17:24:47 -0600

    created inner portion

--- src/module/homePage.js
+++ src/module/homePage.js
@@ -1,7 +1,9 @@
-const mainContent = document.querySelector(".mainContent");
-
 function test() {
+    const mainContent = document.querySelector(".mainContent");
+    const inner = document.getElementById("inner");
+        inner.remove();
     const testing = document.createElement("div");
+        testing.setAttribute("id", "inner");
         testing.style.width = "100px";
         testing.style.height = "100px";
         testing.style.background = "blue";

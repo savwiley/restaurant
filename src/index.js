4d3055f56b5a441711991049974572efca12fe55
Savannaha <savwiley@yahoo.com>
2020-11-17 17:24:01 -0600

    fixed navigation buttons

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import initialPageLoad from "./module/initialPageLoad";
 import homePage from "./module/homePage";
 import menuPage from "./module/menuPage";
-
-run();
+import contactPage from "./module/contactPage";
 
 const run = (() => {
     initialPageLoad();
     homePage();
-    events();
 })();
 
-function events() {
-    const homeBtn = document.getElementById("homeBtn");
-        homeBtn.addEventListener('click', homePage);
-    const menuBtn = document.getElementById("menuBtn");
-        menuBtn.addEventListener('click', menuPage);
-};
+const homeBtn = document.getElementById("homeBtn");
+    homeBtn.addEventListener('click', homePage);
+const menuBtn = document.getElementById("menuBtn");
+    menuBtn.addEventListener('click', menuPage);
+const contactBtn = document.getElementById("contactBtn");
+    contactBtn.addEventListener('click', contactPage);
